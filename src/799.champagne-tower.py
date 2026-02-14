80bcfd415ee7354e606ba55781cb6053e8a970b6
Ahmed El Zainy <ahmedelzainy44@gmail.com>
2026-02-14 11:10:59 +0200

    new updates to the code

---
+++ src/799.champagne-tower.py
@@ -0,0 +1,94 @@
+#
+# @lc app=leetcode id=799 lang=python3
+#
+# [799] Champagne Tower
+#
+# https://leetcode.com/problems/champagne-tower/description/
+
+# algorithms
+# Medium (58.50%)
+# Likes:    3892
+# Dislikes: 216
+# Total Accepted:    190.7K
+# Total Submissions: 315K
+# Testcase Example:  '1\n1\n1'
+#
+# We stack glasses in a pyramid, where the first row has 1 glass, the second
+# row has 2 glasses, and so on until the 100^th row.  Each glass holds one cup
+# of champagne.
+# 
+# Then, some champagne is poured into the first glass at the top.  When the
+# topmost glass is full, any excess liquid poured will fall equally to the
+# glass immediately to the left and right of it.  When those glasses become
+# full, any excess champagne will fall equally to the left and right of those
+# glasses, and so on.  (A glass at the bottom row has its excess champagne fall
+# on the floor.)
+# 
+# For example, after one cup of champagne is poured, the top most glass is
+# full.  After two cups of champagne are poured, the two glasses on the second
+# row are half full.  After three cups of champagne are poured, those two cups
+# become full - there are 3 full glasses total now.  After four cups of
+# champagne are poured, the third row has the middle glass half full, and the
+# two outside glasses are a quarter full, as pictured below.
+# 
+# 
+# 
+# Now after pouring some non-negative integer cups of champagne, return how
+# full the j^th glass in the i^th row is (both i and j are 0-indexed.)
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: poured = 1, query_row = 1, query_glass = 1
+# Output: 0.00000
+# Explanation: We poured 1 cup of champange to the top glass of the tower
+# (which is indexed as (0, 0)). There will be no excess liquid so all the
+# glasses under the top glass will remain empty.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: poured = 2, query_row = 1, query_glass = 1
+# Output: 0.50000
+# Explanation: We poured 2 cups of champange to the top glass of the tower
+# (which is indexed as (0, 0)). There is one cup of excess liquid. The glass
+# indexed as (1, 0) and the glass indexed as (1, 1) will share the excess
+# liquid equally, and each will get half cup of champange.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: poured = 100000009, query_row = 33, query_glass = 17
+# Output: 1.00000
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 0 <= poured <= 10^9
+# 0 <= query_glass <= query_row < 100
+# 
+#
+
+# @lc code=start
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        tower = [[0] * (i + 1) for i in range(100)]
+        tower[0][0] = poured
+        
+        for i in range(99):
+            for j in range(i + 1):
+                excess = max(0, tower[i][j] - 1)
+                tower[i][j] = min(1, tower[i][j])
+                tower[i + 1][j] += excess / 2
+                tower[i + 1][j + 1] += excess / 2
+        
+        return min(1, tower[query_row][query_glass])
+    
+        
+# @lc code=end
+
